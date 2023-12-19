2e92b3ea704c71930f548c47a675e7bc35cb32f9
koisose <plokotokbelajar@gmail.com>
2023-12-19 17:20:42 +0700

    🏠 feat(repo): tambahkan script formatter di semua repo

--- packages/all-about-ai/gemini.mjs
+++ packages/all-about-ai/gemini.mjs
@@ -20,3 +20,32 @@ export async function textGenerate(API_KEY,prompt) {
 
 }
 
+async function run() {
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+    const chat = model.startChat({
+        history: [
+            {
+                role: "user",
+                parts: "Hello, I have 2 dogs in my house.",
+            },
+            {
+                role: "model",
+                parts: "Great to meet you. What would you like to know?",
+            },
+            ],
+        generationConfig: {
+            maxOutputTokens: 100,
+        },
+    });
+
+    const msg = "How many paws are in my house?";
+
+    const result = await chat.sendMessage(msg);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
+
+
